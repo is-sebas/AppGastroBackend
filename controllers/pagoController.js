80ea11259892b86a861e073831272b39ac44c93a
Ricardo <rjgonzalezb@columbia.edu.py>
2023-07-23 22:48:36 -0400

    Procesar pago

--- controllers/pagoController.js
+++ controllers/pagoController.js
@@ -1,4 +1,8 @@
 const Pago = require('../models/pago');
+const OrdersCompart = require('../models/ordersCompart');
+const Order = require('../models/order');
+const UsuariosActivos = require('../models/usuariosActivos');
+const Mesas = require('../models/mesas');
 
 module.exports = {
 
@@ -23,6 +27,44 @@ module.exports = {
             });
 
         });
+    },
+
+    async procesarPago(req, res) {
+
+        const datos = req.body;
+
+        console.log("datos: ", datos);
+        // 1. Actualizamos el estado de ordersCompart 
+        for (const ordersCompart of datos.OrdersCompart) {
+            await OrdersCompart.updateEstado(ordersCompart.id_order_compart, 'PAGADO', (err, id_data) => {
+                if (err) {
+                    return res.status(501).json({
+                        success: false,
+                        message: 'Hubo un error con la actualización del estado de la orden compartida',
+                        error: err
+                    });
+                }
+            });
+        }        
+
+        /*
+        Pago.create(pago, async (err, data) => {
+
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error al momento de registrar el Pago',
+                    error: err
+                });
+            }
+
+            return res.status(201).json({
+                success: true,
+                message: 'El pago se ha registrado correctamente',
+                data: data 
+            });
+
+        });*/
 
     }
 }
--- routes/pagoRoutes.js
+++ routes/pagoRoutes.js
@@ -4,5 +4,6 @@ const passport = require('passport');
 module.exports = (app) => {
 
     app.post('/api/pago/create',  passport.authenticate('jwt', { session: false }), PagoController.create);
+    app.post('/api/pago/procesarPago',  passport.authenticate('jwt', { session: false }), PagoController.procesarPago);
 
 }
