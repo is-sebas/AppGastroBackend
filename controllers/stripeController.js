f9264666989dcf0f4e0dd40a37d7b9e3aaa461fe
Ricardo <rjgonzalezb@columbia.edu.py>
2023-08-05 19:50:03 -0400

    666

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,7 +2,7 @@ const Order = require('../models/order');
 const OrderHasProducts = require('../models/order_has_products');
 const Stripe = require('stripe');
 const stripe = new Stripe('sk_test_51N8s6BLtL5Hr4HtECNY6cvwUNG5vnVjUGCKptsgqlFlpzvERHoZ2BiQe4lupfUwlf75OOFXbMOK9hJ8SP9cbjooN00tt2jWySB');
-const Pago = require('../models/pago');
+const Pago = require('../controllers/pagoController');
 const OrdersCompart = require('../models/ordersCompart');
 
 module.exports = {
@@ -24,25 +24,32 @@ module.exports = {
             console.log('PAYMENT: '+ JSON.stringify(payment, null, 3 ));
             console.log('payment: ', payment);
             console.log('payment.status: ', payment.status);
-            //if (payment !== null) {
-            if (payment.status === 'succeeded') {
+            if (payment !== null) {
+                if (payment.status === 'succeeded') {
 
-                //Realizamos el llamado al endpoind para procesar el pago:
-                Pago.procesarPago(order_compart, (err) => {
-                    if (err) {
-                        return res.status(501).json({
-                            success: false,
-                            message: 'Hubo un error al procesar el pago',
-                            error: err
-                        });
-                    }
-                });
-    
-                return res.status(201).json({
-                    success: true,
-                    message: 'Transaccion exitosa, la orden se ha creado correctamente',
-                    data: `${id}` // EL ID DE LA NUEVA ORDEN
-                });
+                    //Realizamos el llamado al endpoind para procesar el pago:
+                    Pago.procesarPago(order_compart, (err) => {
+                        if (err) {
+                            return res.status(501).json({
+                                success: false,
+                                message: 'Hubo un error al procesar el pago',
+                                error: err
+                            });
+                        }
+                    });
+        
+                    return res.status(201).json({
+                        success: true,
+                        message: 'Transaccion exitosa, la orden se ha creado correctamente',
+                        data: `${id}` // EL ID DE LA NUEVA ORDEN
+                    });
+                }
+                else {
+                    return res.status(501).json({
+                        success: false,
+                        message: 'No se pudo efectuar la transaccion',
+                    });
+                }
             }
             else {
                 return res.status(501).json({
@@ -50,16 +57,9 @@ module.exports = {
                     message: 'No se pudo efectuar la transaccion',
                 });
             }
-            /*}
-            else {
-                return res.status(200).json({
-                    success: false,
-                    message: 'No se pudo efectuar la transaccion',
-                });
-            }*/
 
         } catch (error) {
-            return res.status(200).json({
+            return res.status(501).json({
                 success: false,
                 message: 'No se pudo efectuar la transaccion',
             });
