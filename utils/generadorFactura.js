e565375a610512aa6170fc9d9bbc0ae32d08d86a
Ricardo <rjgonzalezb@columbia.edu.py>
2023-08-31 18:18:04 -0400

    fecha

--- utils/generadorFactura.js
+++ utils/generadorFactura.js
@@ -80,7 +80,7 @@ class generadorFactura {
           <div class="encabezado">
               <h1>Factura</h1>
               <p>Número de Factura: 12345</p>
-              <p>Fecha: 23/08/2023</p>
+              <p>Fecha: 31/08/2023</p>
           </div>
 
           <div class="cliente">
